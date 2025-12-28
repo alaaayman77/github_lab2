00bd4fefa46a984b23d0c602a0118f6c702b5252
alaaayman77 <alaaaymanfouad@gmail.com>
2025-12-28 12:25:51 +0200

    added print hello world method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,13 @@
-. 
+class HelloWorld{
+
+
+    static public void main(String[] args){
+
+    }
+
+   
+    public void printHelloWorld() {
+        System.out.println("Hello World");}
+        
+  
+}
